5126a5611fcded1717f2123dd312e3e673186bdb
KCtops6 <kctops06@gmail.com>
2026-05-07 09:19:53 -0400

    fixed waystone script

--- kubejs/server_scripts/server-events/tags.js
+++ kubejs/server_scripts/server-events/tags.js
@@ -2,7 +2,7 @@
 
 // List of tags: https://minecraft.wiki/w/Item_tag_(Java_Edition) 
 
-ServerEvents.tags('item', event => {
+ServerEvents.tags('block', event => {
     event.add('minecraft:needs_netherite_tool', 'waystones:waystone');
     event.remove('minecraft:needs_diamond_tool', 'waystones:waystone');
     event.remove('minecraft:needs_iron_tool', 'waystones:waystone');
