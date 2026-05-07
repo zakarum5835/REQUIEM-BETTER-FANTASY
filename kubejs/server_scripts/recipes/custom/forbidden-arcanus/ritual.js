fc4c6099e4538e8bdd99e8eceb045380f95935e2
KCtops6 <kctops06@gmail.com>
2026-05-07 13:16:25 -0400

    Fixed missing recipe type for Hephaestus Forge rituals.

--- kubejs/server_scripts/recipes/custom/forbidden-arcanus/ritual.js
+++ kubejs/server_scripts/recipes/custom/forbidden-arcanus/ritual.js
@@ -1,5 +1,9 @@
+// Made by KCtops6
+
 ServerEvents.recipes(event => {
+    // Recipe 1: Cursed Ring
     event.custom({
+        type: "forbidden_arcanus:hephaestus_forge_ritual", // <--- Added this
         additional_requirements: {
             forge_tier: 3
         },
@@ -10,42 +14,12 @@ ServerEvents.recipes(event => {
             souls: 1
         },
         inputs: [
-            {
-                amount: 1,
-                ingredient: {
-                    item: "enigmaticaddons:revival_leaf"
-                }
-            },
-            {
-                amount: 1,
-                ingredient: {
-                    item: "enigmaticlegacy:blazing_core"
-                }
-            },
-            {
-                amount: 1,
-                ingredient: {
-                    item: "enigmaticaddons:ocean_stone"
-                }
-            },
-            {
-                amount: 1,
-                ingredient: {
-                    item: "enigmaticlegacy:angel_blessing"
-                }
-            },
-            {
-                amount: 1,
-                ingredient: {
-                    item: "enigmaticlegacy:eye_of_nebula"
-                }
-            },
-            {
-                amount: 1,
-                ingredient: {
-                    item: "enigmaticlegacy:golem_heart"
-                }
-            }
+            { amount: 1, ingredient: { item: "enigmaticaddons:revival_leaf" } },
+            { amount: 1, ingredient: { item: "enigmaticlegacy:blazing_core" } },
+            { amount: 1, ingredient: { item: "enigmaticaddons:ocean_stone" } },
+            { amount: 1, ingredient: { item: "enigmaticlegacy:angel_blessing" } },
+            { amount: 1, ingredient: { item: "enigmaticlegacy:eye_of_nebula" } },
+            { amount: 1, ingredient: { item: "enigmaticlegacy:golem_heart" } }
         ],
         main_ingredient: {
             item: "enigmaticlegacy:the_cube"
@@ -59,7 +33,9 @@ ServerEvents.recipes(event => {
         }
     });
 
+    // Recipe 2: Twisted Heart
     event.custom({
+        type: "forbidden_arcanus:hephaestus_forge_ritual", // <--- Added this
         additional_requirements: {
             forge_tier: 3
         },
@@ -70,30 +46,10 @@ ServerEvents.recipes(event => {
             souls: 1
         },
         inputs: [
-            {
-                amount: 1,
-                ingredient: {
-                    item: "minecraft:ghast_tear"
-                }
-            },
-            {
-                amount: 2,
-                ingredient: {
-                    item: "minecraft:blaze_powder"
-                }
-            },
-            {
-                amount: 2,
-                ingredient: {
-                    item: "minecraft:redstone"
-                }
-            },
-            {
-                amount: 1,
-                ingredient: {
-                    item: "minecraft:ender_eye"
-                }
-            }
+            { amount: 1, ingredient: { item: "minecraft:ghast_tear" } },
+            { amount: 2, ingredient: { item: "minecraft:blaze_powder" } },
+            { amount: 2, ingredient: { item: "minecraft:redstone" } },
+            { amount: 1, ingredient: { item: "minecraft:ender_eye" } }
         ],
         main_ingredient: {
             item: "enigmaticlegacy:earth_heart"
@@ -107,7 +63,9 @@ ServerEvents.recipes(event => {
         }
     });
 
+    // Recipe 3: Pure Heart
     event.custom({
+        type: "forbidden_arcanus:hephaestus_forge_ritual", // <--- Added this
         additional_requirements: {
             forge_tier: 3
         },
@@ -118,30 +76,10 @@ ServerEvents.recipes(event => {
             souls: 1
         },
         inputs: [
-            {
-                amount: 1,
-                ingredient: {
-                    item: "minecraft:ghast_tear"
-                }
-            },
-            {
-                amount: 2,
-                ingredient: {
-                    item: "enigmaticaddons:ichor_droplet"
-                }
-            },
-            {
-                amount: 2,
-                ingredient: {
-                    item: "minecraft:glowstone_dust"
-                }
-            },
-            {
-                amount: 1,
-                ingredient: {
-                    item: "minecraft:ender_eye"
-                }
-            }
+            { amount: 1, ingredient: { item: "minecraft:ghast_tear" } },
+            { amount: 2, ingredient: { item: "enigmaticaddons:ichor_droplet" } },
+            { amount: 2, ingredient: { item: "minecraft:glowstone_dust" } },
+            { amount: 1, ingredient: { item: "minecraft:ender_eye" } }
         ],
         main_ingredient: {
             item: "enigmaticlegacy:earth_heart"
