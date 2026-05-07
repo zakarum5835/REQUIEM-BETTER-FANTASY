694ed4e725e4db9aacb41a72f6e5749ff70e4027
KCtops6 <kctops06@gmail.com>
2026-05-07 09:07:05 -0400

    removed Netherite Upgrade template from minecraft loot chests

--- kubejs/server_scripts/LootJS/modifiers.js
+++ kubejs/server_scripts/LootJS/modifiers.js
@@ -1,3 +1,4 @@
 LootJS.modifiers(event => {
     event.addLootTableModifier(/^twilightforest:chests\/.*/).removeLoot('minecraft:blaze_rod');
+    event.addLootTableModifier(/^minecraft:chests\/.*/).removeLoot('minecraft:netherite_upgrade_smithing_template');
 });
---
+++ kubejs/server_scripts/server-events/tags.js
@@ -0,0 +1,7 @@
+// Made by KCtops6
+
+// List of tags: https://minecraft.wiki/w/Item_tag_(Java_Edition) 
+
+ServerEvents.tags('item', event => {
+    event.add('minecraft:needs_netherite_tool', 'waystones:waystone');
+});
