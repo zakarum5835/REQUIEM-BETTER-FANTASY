52b7c1a6b3e5b34babb41297293d1ef7fe12f41a
zakarum5835 <demonshit4@gmail.com>
2026-05-07 14:27:49 +0200

    Delete kubejs/server_scripts directory

--- kubejs/server_scripts/drops/swampjaw_drop.js
+++
@@ -1,5 +0,0 @@
-//Made by zakarum5835
-
-EntityEvents.drops('meetyourfight:swampjaw', event => {
-    event.addDrop(Item.of('additionaladditions:rose_gold_upgrade').withCount(1).withChance(0.5))
-})
