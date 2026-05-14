fd24588181dbb6d91fe18ae8652f477fa71656c6
KCtops6 <kctops06@gmail.com>
2026-05-14 10:25:50 -0400

    debug version of flint pickaxes script

--- kubejs/startup_scripts/item-events/modification/flint-pickaxe.js
+++ kubejs/startup_scripts/item-events/modification/flint-pickaxe.js
@@ -1,9 +1,24 @@
 // Made by KCtops6
+// Debug version for troubleshooting modpack compatibility
 
 ItemEvents.modification(event => {
-    event.modify('notreepunching:flint_pickaxe', item => {
-        item.setTier(tier => {
-            tier.level = 0;
-        });
-    });
+    console.info('KubeJS: Item modification script started...');
+    const modifyTier = (itemID, newLevel) => {
+        try {
+            console.info(`KubeJS: Attempting to modify tier for [${itemID}] to level ${newLevel}`);
+            
+            event.modify(itemID, item => {
+                item.setTier(tier => {
+                    tier.level = newLevel;
+                });
+            });
+            
+            console.info(`KubeJS: Successfully applied modification to [${itemID}]`);
+        } catch (err) {
+            console.error(`KubeJS: Failed to modify [${itemID}]. Error: ${err}`);
+        }
+    };
+    modifyTier('notreepunching:flint_pickaxe', 0);
+    modifyTier('minecraft:netherite_pickaxe', 0);
+    console.info('KubeJS: Item modification script finished.');
 });
