004d6b43c159e9f61dcbbe640aea8ec1ea8cd1e2
KCtops6 <kctops06@gmail.com>
2026-05-14 11:22:53 -0400

    got rid of debuging messages

--- kubejs/startup_scripts/item-events/modification/flint-pickaxe.js
+++ kubejs/startup_scripts/item-events/modification/flint-pickaxe.js
@@ -2,23 +2,12 @@
 // Debug version for troubleshooting modpack compatibility
 
 ItemEvents.modification(event => {
-    console.info('KubeJS: Item modification script started...');
-    console.info("CHECKING MODS: No Tree Punching loaded? " + Platform.isModLoaded('notreepunching'));
     const modifyTier = (itemID, newLevel) => {
-        try {
-            console.info(`KubeJS: Attempting to modify tier for [${itemID}] to level ${newLevel}`);
-            
-            event.modify(itemID, item => {
-                item.setTier(tier => {
-                    tier.level = newLevel;
-                });
+        event.modify(itemID, item => {
+            item.setTier(tier => {
+                tier.level = newLevel;
             });
-            
-            console.info(`KubeJS: Successfully applied modification to [${itemID}]`);
-        } catch (err) {
-            console.error(`KubeJS: Failed to modify [${itemID}]. Error: ${err}`);
-        }
+        });
     };
     modifyTier('notreepunching:flint_pickaxe', 0);
-    console.info('KubeJS: Item modification script finished.');
 });
