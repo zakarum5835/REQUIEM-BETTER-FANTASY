fdac59f1128a8fc98481050e16195a31da1018c8
KCtops6 <kctops06@gmail.com>
2026-05-15 13:50:03 -0400

    removed infernal tome drop from heretic

--- kubejs/server_scripts/LootJS/modifiers.js
+++ kubejs/server_scripts/LootJS/modifiers.js
@@ -1,4 +1,7 @@
+// Made by KCtops6
+
 LootJS.modifiers(event => {
     event.addLootTableModifier(/^twilightforest:chests\/.*/).removeLoot('minecraft:blaze_rod');
     event.addLootTableModifier(/^minecraft:chests\/.*/).removeLoot('minecraft:netherite_upgrade_smithing_template');
+    event.addEntityLootModifier('goety:heretic').removeLoot('goety:infernal_tome');
 });
