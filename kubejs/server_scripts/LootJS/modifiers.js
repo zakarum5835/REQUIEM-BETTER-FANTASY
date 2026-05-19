f79eedb15224b8b6977e8d15a5c96a83d5ceb034
KCtops6 <kctops06@gmail.com>
2026-05-19 09:52:29 -0400

    brute force solution to waystones problem

--- kubejs/server_scripts/LootJS/modifiers.js
+++ kubejs/server_scripts/LootJS/modifiers.js
@@ -4,6 +4,22 @@ LootJS.modifiers(event => {
     event.addLootTableModifier(/^twilightforest:chests\/.*/).removeLoot('minecraft:blaze_rod');
     event.addLootTableModifier(/^minecraft:chests\/.*/).removeLoot('minecraft:netherite_upgrade_smithing_template');
     event.addEntityLootModifier('goety:heretic').removeLoot('goety:infernal_tome');
+    const NETHERITE_LEVEL_PICKAXES = [
+        'minecraft:netherite_pickaxe', 'mekanismtools:refined_obsidian_pickaxe', 'terramity:dimlite_pickaxe',
+        'terramity:virentium_pickaxe', 'terramity:cosmilite_pickaxe', 'terramity:iridium_pickaxe',
+        'terramity:void_pickaxe', 'terramity:conductite_pickaxe', 'terramity:hellspec_pickaxe',
+        'terramity:nyxium_pickaxe', 'terramity:exodium_pickaxe', 'terramity:reverium_pickaxe',
+        'voidscape:voidic_crystal_pickaxe', 'voidscape:titanite_pickaxe', 'voidscape:ichor_pickaxe',
+        'voidscape:astral_pickaxe', 'additionaladditions:gilded_netherite_pickaxe', 'goety:dark_pickaxe',
+        'forbidden_arcanus:draco_arcanus_pickaxe', 'blazegear:nethersteel_pickaxe', 'enigmaticlegacy:etherium_pickaxe',
+        'theabyss:aberythe_pickaxe', 'theabyss:fusion_pickaxe', 'theabyss:phantom_pickaxe',
+        'theabyss:garnite_pickaxe', 'mysticalagriculture:tertium_pickaxe', 'mysticalagriculture:imperium_pickaxe',
+        'mysticalagriculture:supremium_pickaxe', 'mysticalagriculture:awakened_supremium_pickaxe', 'macabre:blood_clot_pickaxe',
+        'macabre:plasma_pickaxe', 'macabre:ferrum_pickaxe', 'macabre:abhorrent_pickaxe',
+        'twilightforest:fiery_pickaxe', 'majruszsdifficulty:enderium_pickaxe', 'ancient_elements:aeonite_pickaxe', 
+        'ancient_elements:galactrium_pickaxe', 'ancient_elements:celestium_pickaxe', 'ancient_elements:endrium_pickaxe',
+        'ancient_elements:spectrillium_pickaxe'
+    ];
     const WAYSTONE_BLOCKS = [
         'waystones:waystone', 'waystones:mossy_waystone', 'waystones:sandy_waystone',
         'waystones:deepslate_waystone', 'waystones:blackstone_waystone', 'waystones:end_stone_waystone',
@@ -25,8 +41,8 @@ LootJS.modifiers(event => {
                     ctx.loot.clear();
                     return;
                 }
-                if (item.id === 'minecraft:netherite_pickaxe') return;
-                if (item.hasTag('minecraft:mineable/pickaxe') && !item.hasTag('minecraft:incorrect_for_diamond_tool')) return;
+                const isItemAllowed = NETHERITE_LEVEL_PICKAXES.includes(String(item.id));
+                if (isItemAllowed) return; 
                 ctx.loot.clear();
             });
     });
