ad08d1e4ea7bcbcfb6e3021e0d256a09eadcdec6
KCtops6 <kctops06@gmail.com>
2026-05-21 12:41:21 -0400

    added goety faceless mob summoning

--- kubejs/server_scripts/recipes/custom/goety/ritual.js
+++ kubejs/server_scripts/recipes/custom/goety/ritual.js
@@ -27,4 +27,34 @@ ServerEvents.recipes(event => {
             item: "goety:jei_dummy/none"
         }
     });
+
+    event.custom({
+        type: "goety:ritual",
+        ritual_type: "goety:summon",
+        activation_item: {
+            item: "eldritch_end:aberration_heart"
+        },
+        craftType: "sabbath",
+        entity_to_summon: "eldritch_end:the_faceless",
+        soulCost: 1,
+        duration: 30,
+        dimension: "minecraft:the_end",
+        ingredients: [
+            {
+                item: "eldritch_end:ominous_eye"
+            },
+            {
+                item: "goety:void_block"
+            },
+            {
+                item: "minecraft:black_candle"
+            },
+            {
+                item: "fdbosses:lightning_core"
+            }
+        ],
+        result: {
+            item: "goety:jei_dummy/none"
+        }
+    });
 });
