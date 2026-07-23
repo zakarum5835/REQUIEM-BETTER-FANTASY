d2e69d0a5266f02159a9424d15fa3f1c7b5dc598
zakarum5835 <demonshit4@gmail.com>
2026-07-24 01:25:41 +0200

    Delete kubejs directory

--- kubejs/client_scripts/tooltip.js
+++
@@ -1,5 +0,0 @@
-ItemEvents.tooltip(event => {
-    // Adds text to a single item
-    event.add('distant_worlds:charged_wither_totem', 'Do not use this item without obtaining the Zaka Star, I warned you.')
-    
-})
